f9450d37c3de4423960f3f0b536e90c35a26d4e9
253492569@qq.com <253492569@qq.com>
2020-11-05 22:51:41 +0800

    ‘新增测试项目’

---
+++ 测试/测试/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by admin on 2020/11/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 测试/测试/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  测试
+//
+//  Created by admin on 2020/11/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
